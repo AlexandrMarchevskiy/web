ce9c79fe9a0b37c9b39a9a8042259a6ef138afee
AlexandrMarchevskiy <alexander.marchevsky@yandex.ru>
2021-12-31 21:44:08 +0300

    nginx+gunicorn

--- init.sh
+++ init.sh
@@ -1,2 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+# Gunicorn (ver. 17.5)
+touch /home/box/gunicorn.log
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/ask
+sudo /etc/init.d/gunicorn restart
